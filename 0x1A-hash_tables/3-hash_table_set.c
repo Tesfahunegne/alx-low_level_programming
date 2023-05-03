fdad93a3632d42dba2b3065865b3b2425238dde9
Tesfahunegne <tsweetam@gmail.com>
2023-05-03 02:26:38 -0700

    my commit

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,15 +1,41 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include "hash_tables.h"
 
 /**
-  * key_index - Computes the index of a given key in a hash table
-  * @key: The given key
-  * @size: The size of the hash table
+  * hash_table_set - Adds an element to the hash table
+  * @ht: The hash table to add or update the key/value to
+  * @key: The key of a value
+  * @value: The value associated with the key
   *
-  * Return: The index of a key in a hash table
+  * Return: 1 if it succeeded, 0 otherwise
   */
-unsigned long int key_index(const unsigned char *key, unsigned long int size)
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	return (hash_djb2(key) % size);
+	unsigned long int idx = 0;
+	hash_node_t *elem = NULL, *new_node = NULL;
+
+	if (ht == NULL || key == NULL || (strcmp(key, "") == 0))
+		return (0);
+
+	idx = key_index((unsigned char *) key, ht->size);
+	elem = ht->array[idx];
+
+	if (elem && strcmp(key, elem->key) == 0)
+	{
+		free(elem->value);
+		elem->value = strdup(value);
+		return (1);
+	}
+
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+		return (0);
+
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+	new_node->next = ht->array[idx];
+	ht->array[idx] = new_node;
+	return (1);
 }
